a1c8d02d96b6795f26672fdc4a7e314e51c442dd
Zhengqbbb <1074059947@qq.com>
2022-08-25 10:35:24 +0800

    chore: update config

--- scripts/commitizen.cjs
+++ scripts/commitizen.cjs
@@ -1,15 +1,15 @@
 /** @type {import('cz-git').CommitizenGitOptions} */
 module.exports = {
-  // @use termianl like: `czg :i`
+  // @use termianl like: `git czg :i`
   alias: {
+    ':': 'docs: update posts',
+    'f': 'docs: fix typos',
+    'r': 'docs: update README',
     'a': 'feat(theme): update page theme',
     'b': 'build: bump dependencies',
     's': 'style: update code format',
     'i': 'chore: initial commit',
     'c': 'chore: update config',
-    'f': 'docs: fix typos',
-    'r': 'docs: update README',
-    ':': 'docs: update posts',
   },
   scopes: ['theme', 'posts'],
   customScopesAlign: 'top',
