aef933103aa94562f2bb2303a6547a294438f652
Zhengqbbb <1074059947@qq.com>
2022-09-20 01:50:29 +0800

    feat(theme): add page base setting

--- build/plugins/pwa.ts
+++ build/plugins/pwa.ts
@@ -1,5 +1,9 @@
 import { VitePWA } from 'vite-plugin-pwa'
+import fg from 'fast-glob'
+import { resolve } from 'pathe'
 
+// eslint-disable-next-line prefer-regex-literals
+const __GITHUB_SOURCE_CONTENT_REGEX = new RegExp('^https://(((raw|user-images|camo).githubusercontent.com))/.*', 'i')
 /**
  * Vite Plugin PWA uses Workbox  library to build the service worker
  * can find more information on Workbox section.
@@ -7,12 +11,17 @@ import { VitePWA } from 'vite-plugin-pwa'
  */
 export const pwaPlugin = VitePWA({
   registerType: 'autoUpdate',
-  includeAssets: ['favicon.svg', 'safari-pinned-tab.svg'],
+  includeAssets: fg.sync('**/*.{png,svg,gif,ico,txt}', { cwd: resolve(__dirname, '../../public') }),
   manifest: {
-    name: 'Vitesse',
-    short_name: 'Vitesse',
-    theme_color: '#ffffff',
+    name: 'Q.Ben',
+    short_name: 'Q.Ben',
+    theme_color: '#050505',
     icons: [
+      {
+        src: '/icons/apple-touch-120x120.png',
+        sizes: '120x120',
+        type: 'image/png',
+      },
       {
         src: '/pwa-192x192.png',
         sizes: '192x192',
@@ -24,10 +33,30 @@ export const pwaPlugin = VitePWA({
         type: 'image/png',
       },
       {
-        src: '/pwa-512x512.png',
-        sizes: '512x512',
-        type: 'image/png',
+        src: '/logo-dark.svg',
+        sizes: '165x165',
+        type: 'image/svg',
         purpose: 'any maskable',
+      },
+    ],
+  },
+  workbox: {
+    navigateFallbackDenylist: [/^\/new$/],
+    globPatterns: ['**/*.{css,js,html,png,svg,gif,ico,woff2}'],
+    runtimeCaching: [
+      {
+        urlPattern: __GITHUB_SOURCE_CONTENT_REGEX,
+        handler: 'CacheFirst',
+        options: {
+          cacheName: 'githubusercontent-images-cache',
+          expiration: {
+            maxEntries: 10,
+            maxAgeSeconds: 60 * 60 * 24 * 365, // <== 365 days
+          },
+          cacheableResponse: {
+            statuses: [0, 200],
+          },
+        },
       },
     ],
   },
--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,26 @@
 import { isClient } from '~/utils'
 
 useHead({
-  title: 'Q.Ben | Zhengqbbb',
+  title: 'Q.Ben Zheng | Zhengqbbb',
   meta: [
-    { name: 'description', content: 'Qbenben\'s personal website' },
-    { name: 'theme-color', content: computed(() => isDark.value ? '#00aba9' : '#ffffff') },
+    { name: 'description', content: 'Q.Ben Zheng\'s Personal Website' },
+    { name: 'keywords', content: 'Q.Ben, qbb, Zhengqbbb, Qbenben' },
+    { name: 'author', content: 'Zhengqbbb' },
+    { property: 'og:title', content: 'Q.Ben Zheng' },
+    { property: 'og:image', content: 'https://qbb.sh/avatar.jpeg' },
+    { name: 'twitter:card', content: 'summary' },
+    { name: 'twitter:creator', content: '@zhengqbbb' },
+    { name: 'application-name', content: 'Q.Ben' },
+    { name: 'apple-mobile-web-app-title', content: 'Q.Ben | Zhengqbbb' },
+    { name: 'apple-mobile-web-app-status-bar-style', content: 'default' },
+    { name: 'theme-color', content: computed(() => isDark.value ? '#050505' : '#ffffff') },
   ],
   link: [
-    {
-      rel: 'icon',
-      type: 'image/svg+xml',
-      href: '/favicon.png',
-    },
+    { rel: 'shortcut icon', href: '/favicon.ico' },
+    { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+    { rel: 'icon', type: 'image/svg+xml', href: '/logo-dark.svg' },
+    { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#aaaaaa' },
+    { rel: 'apple-touch-icon', sizes: '120x120', href: '/icons/apple-touch-120x120.png' },
   ],
 })
 
