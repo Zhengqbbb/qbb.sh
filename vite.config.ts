9f77ac900180bdae329b38a03079d5b920c6fd56
Zhengqbbb <1074059947@qq.com>
2022-10-13 11:44:06 +0800

    chore: update config

--- vite.config.ts
+++ vite.config.ts
@@ -1,41 +1,78 @@
 import { resolve } from 'pathe'
 import { defineConfig } from 'vite'
-import generateSitemap from 'vite-ssg-sitemap'
+import Vue from '@vitejs/plugin-vue'
+import Pages from 'vite-plugin-pages'
 import Layouts from 'vite-plugin-vue-layouts'
 import Markdown from 'vite-plugin-vue-markdown'
-import UnoCSS from 'unocss/vite'
 import Inspect from 'vite-plugin-inspect'
+import generateSitemap from 'vite-ssg-sitemap'
+import { VitePWA } from 'vite-plugin-pwa'
+import UnoCSS from 'unocss/vite'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
 import {
-  autoImportPlugin,
-  componentsPlugin,
-  pagesPlugin,
-  pwaPlugin,
   registerMarkdownPlugins,
-  vuePlugin,
+  resolvePostFile,
+  resolvePostList,
+  vitePWAOptions,
 } from './build'
 
 export default defineConfig({
   resolve: {
     alias: [
       { find: '~/', replacement: `${resolve(__dirname, 'src')}/` },
+      { find: '~build/', replacement: `${resolve(__dirname, 'build')}/` },
     ],
   },
 
   plugins: [
     UnoCSS(),
-    vuePlugin,
-    pagesPlugin,
-    autoImportPlugin,
-    componentsPlugin,
-    pwaPlugin,
+    Vue({
+      include: [/\.vue$/, /\.md$/],
+      reactivityTransform: true,
+    }),
 
-    /** @see https://github.com/JohnCampionJr/vite-plugin-vue-layouts */
+    // https://github.com/hannoeru/vite-plugin-pages
+    Pages({
+      pagesDir: 'pages',
+      extensions: ['vue', 'md'],
+      extendRoute: route => resolvePostFile(route),
+      onRoutesGenerated: routes => resolvePostList(routes),
+    }),
+
+    // https://github.com/antfu/unplugin-auto-import
+    AutoImport({
+      imports: [
+        'vue',
+        'vue-router',
+        'vue/macros',
+        '@vueuse/head',
+        '@vueuse/core',
+      ],
+      vueTemplate: true,
+      dirs: [
+        'src/composables',
+      ],
+      dts: 'src/auto-imports.d.ts',
+    }),
+
+    // https://github.com/antfu/unplugin-vue-components#usage && https://github.com/johnsoncodehk/volar/discussions/471
+    Components({
+      extensions: ['vue', 'md'],
+      include: [/\.vue$/, /\.vue\?vue/, /\.md$/],
+      dirs: 'src/components',
+      dts: 'src/components.d.ts',
+    }),
+
+    VitePWA(vitePWAOptions),
+
+    // https://github.com/JohnCampionJr/vite-plugin-vue-layouts
     Layouts(),
 
-    /** @see https://github.com/antfu/vite-plugin-inspect || @see http://localhost:3333/__inspect/ */
+    // https://github.com/antfu/vite-plugin-inspect || http://localhost:3333/__inspect/
     Inspect(),
 
-    /** @see https://github.com/antfu/vite-plugin-vue-markdown */
+    // https://github.com/antfu/vite-plugin-vue-markdown
     Markdown({
       wrapperClasses: 'prose m-auto',
       headEnabled: true,
@@ -46,7 +83,7 @@ export default defineConfig({
     }),
   ],
 
-  /** @see https://github.com/antfu/vite-ssg */
+  // https://github.com/antfu/vite-ssg
   ssgOptions: {
     script: 'async',
     formatting: 'minify',
@@ -54,6 +91,7 @@ export default defineConfig({
     onFinished() {
       generateSitemap({
         hostname: 'https://www.qbb.sh',
+        exclude: ['/mdtest'],
       })
     },
   },
