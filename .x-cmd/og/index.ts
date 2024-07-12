0e19675e57e489f0b4757d20e7b25d4d4e5349aa
Zhengqbbb <1074059947@qq.com>
2024-07-12 10:30:09 +0800

    feat(og): using x-satori replace tsx to gen open-gragh

--- .x-cmd/og/index.ts
+++ .x-cmd/og/index.ts
@@ -2,64 +2,62 @@ import { dirname } from 'node:path'
 import { fileURLToPath } from 'node:url'
 import { readFile, writeFile } from 'node:fs/promises'
 import { resolve } from 'pathe'
-import type { SatoriOptions } from 'satori'
-import satori from 'satori'
 import { Resvg, renderAsync } from '@resvg/resvg-js'
-import template from './template'
+import { satoriAstro } from 'x-satori/astro'
+import { experimental_AstroContainer as AstroContainer } from 'astro/container'
 
 const _DIRNAME = dirname(fileURLToPath(import.meta.url))
-/**
- * That is why I don't use Astro file endpoints to gen OpenGraph image.
- * It look like designed for SSR more, not for SSG.
- * Using build end hook replace it.
- */
-// const _DIRNAME = import.meta?.env?.PROD
-//     ? resolve(dirname(fileURLToPath(import.meta.url)), '../../../../src/lib/server/og')
-//     : dirname(fileURLToPath(import.meta.url))
-
-export const baseSatoriOpts: SatoriOptions = {
-    width: 1200,
-    height: 628,
-    fonts: [
-        {
-            name: 'Inter',
-            data: await readFile(resolve(_DIRNAME, './font/Inter-Medium.woff')),
-            weight: 400,
-            style: 'normal',
-        },
-        {
-            name: 'Inter',
-            data: await readFile(resolve(_DIRNAME, './font/Inter-Bold.woff')),
-            weight: 700,
-            style: 'normal',
-        },
-        {
-            name: 'Noto Sans SC',
-            data: await readFile(resolve(_DIRNAME, './font/NotoSansSC-Medium.otf')),
-            weight: 400,
-            style: 'normal',
-        },
-        {
-            name: 'Noto Sans SC',
-            data: await readFile(resolve(_DIRNAME, './font/NotoSansSC-Bold.otf')),
-            weight: 700,
-            style: 'normal',
-        },
-        {
-            name: 'Noto Sans Symbols',
-            data: await readFile(resolve(_DIRNAME, './font/NotoSansSymbols2-Regular.ttf')),
-            weight: 700,
-            style: 'normal',
-        },
-    ],
-}
 
 export async function genSVG(
     title?: string,
     desc?: string,
     site?: string,
 ) {
-    return await satori(template(title, desc, site), baseSatoriOpts)
+    const template = await readFile(resolve(_DIRNAME, './Template.astro'), 'utf-8')
+    return await satoriAstro(
+        {
+            props: {
+                title,
+                desc,
+                site,
+            },
+            width: 1200,
+            height: 628,
+            fonts: [
+                {
+                    name: 'Inter',
+                    data: await readFile(resolve(_DIRNAME, './font/Inter-Medium.woff')),
+                    weight: 400,
+                    style: 'normal',
+                },
+                {
+                    name: 'Inter',
+                    data: await readFile(resolve(_DIRNAME, './font/Inter-Bold.woff')),
+                    weight: 700,
+                    style: 'normal',
+                },
+                {
+                    name: 'Noto Sans SC',
+                    data: await readFile(resolve(_DIRNAME, './font/NotoSansSC-Medium.otf')),
+                    weight: 400,
+                    style: 'normal',
+                },
+                {
+                    name: 'Noto Sans SC',
+                    data: await readFile(resolve(_DIRNAME, './font/NotoSansSC-Bold.otf')),
+                    weight: 700,
+                    style: 'normal',
+                },
+                {
+                    name: 'Noto Sans Symbols',
+                    data: await readFile(resolve(_DIRNAME, './font/NotoSansSymbols2-Regular.ttf')),
+                    weight: 700,
+                    style: 'normal',
+                },
+            ],
+        },
+        template,
+    )
 }
 
 /** For mian.ts */
@@ -80,7 +78,6 @@ export async function genPNG(
     return await writeFile(output, render.asPng())
 }
 
-/** For astro gen */
 export async function genPNGBuffer(
     title?: string,
     desc?: string,
